dce18a58e0edf620cb1f7bda23714ff7a70a02a4
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-03-19 22:12:33 -0400

    Add user name and age function

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,5 @@
 puts "What is your name?"
+user_name = gets.chomp
+puts "Hello, #{user_name}. What year were you born?"
+user_age = 2018 - gets.chomp.to_i
+puts "Oh wow #{user_name}, you are #{user_age}!"
